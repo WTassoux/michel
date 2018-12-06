cf9e20cfb3cba5cd4a2f7fecdaa11ee6209cf380
Olivier COMETTO <olivier.cometto@amadeus.com>
2018-12-06 16:59:00 +0100

    adding new hyperparameters to dataframe_output

--- dcm.py
+++ dcm.py
@@ -16,6 +16,7 @@ import glob
 import pandas
 import numpy
 import math
+from datetime import datetime, timedelta
 
 
 # We check if more data is available online
@@ -79,6 +80,10 @@ def glickoRanking(df, period, delta_volatility):
     the glicko ranking of the 2 players at the beginning of the match
     The period is given in days
     The delta_volatility is a constant to constrain the change of volatility over time
+
+    The selected period is 7 days, following the ATP ranking updates
+    On each pass, all rankings are updated: for player who do not play, their RD increases
+
     
     """
     print("Glicko-2 rankings computing...")
@@ -196,3 +201,155 @@ def new_sigma_function(sigma, delta, phi, variance, tau, error):
 def sigma_convergence_function(x, sigma, delta, phi, variance, tau, error):
     a=math.log(sigma**2)
     return ((math.exp(x)*(delta**2-phi**2-variance-math.exp(x)))/(2*(phi**2+variance+math.exp(x))**2)-(x-a)/tau**2)
+
+
+def features_past_generation(features_creation_function,
+                             days,
+                             feature_names_prefix,
+                             data,
+                             indices):
+    """
+    Creates features based on the past of the players. 
+    Basically a for loop. Takes 1 match at a time, selects the matches that occurred during 
+    its close past (usually 150 days before max) and computes some features.
+    Each match will appear twice in the dataset : 1 time per outcome of the match.
+    Example : 02/03/2016 Djoko-Zverev ,Djoko won
+        During the 150 days before the match, Djoko won 80% of its matches and Zverev 40%.
+        We encode the outcome "Djoko wins" like that : [80,40], and tell the model this outcome happened (1).
+        We encode the outcome "Zverev wins" like that : [40,80], and tell the model it didn't happen (0).
+    And we do that with some more features , based on the players past stats on the surface
+    of the match, on the recent injuries, ...
+    In the inputs of the function, "indices" contains the indices of the matches we want to encode.
+    The output of the functions is twice as long as "indices".
+    (these features introduce many hyperparameters to be tuned...)
+    """
+    matches_outcomes=[]
+    for i,match_indice in enumerate(indices):
+        match=data.iloc[match_indice,:]
+        past_matches=data[(data.Date<match.Date)&(data.Date>=match.Date-timedelta(days=days))]
+        match_features_outcome_1=features_creation_function(1,match,past_matches)
+        match_features_outcome_2=features_creation_function(2,match,past_matches)
+        matches_outcomes.append(match_features_outcome_1)
+        matches_outcomes.append(match_features_outcome_2)
+        if i%500==0:
+            print(str(i)+"/"+str(len(indices))+" matches treated.")
+    train=pandas.DataFrame(matches_outcomes)
+    train.columns=[feature_names_prefix+str(i) for i in range(len(train.columns))]
+    return train
+
+
+def features_player_creation(outcome,match,past_matches):
+    features_player=[]
+    """
+    8 features added by this function:
+    1. Number of matches won
+    2. Number of matches lost
+    3. Total number of matches played
+    4. Percentage of matches won
+    5. Number of matches won on that surface
+    6. Number of matches lost on that surface
+    7. Total number of matches played on that surface
+    8. Percentage of matches won on that surface
+    """
+    ##### Match information extraction (according to the outcome)
+    player=match.Winner if outcome==1 else match.Loser
+    surface=match.Surface
+    ##### General stats
+    wins=past_matches[past_matches.Winner==player]    
+    losses=past_matches[past_matches.Loser==player]    
+    todo=pandas.concat([wins,losses],0)
+    features_player+=[len(wins),len(losses),len(todo)]
+    per_victory=100*len(wins)/len(todo) if len(todo)>0 else numpy.nan
+    features_player.append(per_victory)
+    ##### Surface
+    past_surface=past_matches[past_matches.Surface==surface]
+    wins_surface=past_surface[past_surface.Winner==player]    
+    losses_surface=past_surface[past_surface.Loser==player]    
+    todo_surface=pandas.concat([wins_surface,losses_surface],0)
+    features_player+=[len(wins_surface),len(losses_surface),len(todo_surface)]
+    per_victory_surface=100*len(wins_surface)/len(todo_surface) if len(todo_surface)>0 else numpy.nan
+    features_player.append(per_victory_surface)
+    return features_player
+
+def features_recent_creation(outcome,match,past_matches):
+    """
+    7 features added by this function:
+    1. Days since last match
+    2. Was the last match won ?
+    3. Ranking of the last player played
+    4. Number of sets of last match played
+    5. Number of sets won during last match played
+    6. Did the player finish the previous match? (did not retire)
+    7. Was the player injued in the specified period?
+    """
+    ##### Match information extraction (according to the outcome)
+    player=match.Winner if outcome==1 else match.Loser
+    date=match.Date
+    ##### Last matches
+    wins=past_matches[past_matches.Winner==player]    
+    losses=past_matches[past_matches.Loser==player]    
+    todo=pandas.concat([wins,losses],0)
+    if len(todo)==0:
+        return [numpy.nan]*7
+    # Days since last match
+    dslm=(date-todo.iloc[-1,:].Date).days
+    # Was the last match won ?
+    wlmw=int(todo.iloc[-1,:].Winner==player)
+    # Ranking of the last player played
+    rlpp=todo.iloc[-1,:].WRank
+    # Number of sets of last match played
+    nslmp=todo.iloc[-1,:]['Best of']
+    # Number of sets won during last match played
+    nswlmp=todo.iloc[-1,:]['Wsets'] if wlmw==1 else todo.iloc[-1,:]['Lsets']
+    # Injuries - iitp + injury last match
+    if len(losses)!=0:
+        ilm=int(losses.iloc[-1,:].Comment=="Completed")
+        iitp=1 if (losses.Comment!="Completed").sum()>0 else 0
+    else:
+        ilm=numpy.nan
+        iitp=numpy.nan
+    features_recent=[dslm,wlmw,rlpp,nslmp,nswlmp,ilm,iitp]
+    return features_recent
+
+def features_h2h_creation(outcome,match,past):
+    """
+    4 features added by this function:
+    1. Total number of matches played between these 2 players in the past period
+    2. Number of matches won
+    3. Number of matches lost
+    4. Percentage of won matches in the H2H
+    """
+    features_h2h=[]
+    ##### Match information extraction (according to the outcome)
+    player1=match.Winner if outcome==1 else match.Loser
+    player2=match.Loser if outcome==1 else match.Winner
+    ##### General h2h features
+    # % of the previous matches between these 2 players won by each.
+    h2h1=past[(past.Winner==player1)&(past.Loser==player2)]    
+    h2h2=past[(past.Winner==player2)&(past.Loser==player1)]    
+    h2h=pandas.concat([h2h1,h2h2],0)
+    features_h2h+=[len(h2h),len(h2h1),len(h2h2)]
+    per_victory_player1=100*len(h2h1)/len(h2h) if len(h2h)>0 else numpy.nan
+    features_h2h.append(per_victory_player1)
+    return features_h2h
+
+def features_general_creation(outcome,match,past_matches):
+    """
+    3 features added by this function:
+    1. Difference in ATP rank between the winner and the loser
+    2. Did the best ranked person win?
+    3. Best player ranking
+    """
+    features_general=[]
+    ##### Match information extraction (according to the outcome)
+    player1=match.Winner if outcome==1 else match.Loser
+    rank_player_1=match.WRank if outcome==1 else match.LRank
+    rank_player_2=match.LRank if outcome==1 else match.WRank
+    
+    features_general+=[rank_player_2-rank_player_1,
+                       int(rank_player_1>rank_player_2)]
+    best_ranking_as_winner=past_matches[(past_matches.Winner==player1)].WRank.min()
+    best_ranking_as_loser=past_matches[(past_matches.Loser==player1)].LRank.min()
+    best_ranking=min(best_ranking_as_winner,best_ranking_as_loser)
+    features_general.append(best_ranking)
+    return features_general
--- main.py
+++ main.py
@@ -7,18 +7,54 @@
 
 from dcm import *
 from testfunctions import *
+from datetime import datetime,timedelta
 
+########################################################
 ### TODO ### First we need to retrieve the latest data
 #dataScrapper()
 
+
+
+###############################################################
 # We now clean the data and keep only the hyperparameters we need
 df=dataCleaner()
 
+
+
+####################################################################
 # We compute the custom ranking:the period is currently 1 day as it is not taken into account at all
 #df=glickoRanking(df, 1, 0.5)
 df=compute_elo_rankings(df)
 
-df.to_csv('dataframe_output.csv', sep=',', encoding='utf-8')
 
-print(testRankingAccuracy(df,'WRank','LRank'))
-print(testRankingAccuracy(df,'elo_loser','elo_winner'))
+
+
+df.to_csv('dataframe_output.csv', sep=',', encoding='utf-8')
+# Percentage of matches with correct prediction from the rankings
+print("Accuracy of ATP ranking for match outcome prediction: "+str(testRankingAccuracy(df,'WRank','LRank')))
+print("Accuracy of ELO ranking for match outcome prediction: "+str(testRankingAccuracy(df,'elo_loser','elo_winner')))
+
+
+
+#############################################
+# Generation of additional hyper parameters
+
+beg = datetime(2008,1,1) 
+#beg = df.Date.iloc[0]
+end = df.Date.iloc[-1]
+indices = df[(df.Date>beg)&(df.Date<=end)].index
+
+
+# Param based on past data (for a match, will look at the past x days except for the player where we check only the past 5 days)
+x = 150
+print("### Computing basic player data ###")
+features_player  = features_past_generation(features_player_creation,5,"playerft5",df,indices)
+print("### Computing basic match data ###")
+features_h2h     = features_past_generation(features_duo_creation,x,"h2hft",df,indices)
+print("### Computing recent data about matches ###")
+features_general = features_past_generation(features_general_creation,x,"generalft",df,indices)
+print("### Computing basic player data ###")
+features_recent  = features_past_generation(features_recent_creation,x,"recentft",df,indices)
+
+
+print("Done!")
