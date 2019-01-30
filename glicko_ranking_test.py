be02fc63b9d2cf86c43264e2c5005eb131ad0f21
Olivier COMETTO <Olivier.COMETTO@amadeus.com>
2019-01-30 18:50:15 +0100

    testing glicko

--- glicko_ranking_test.py
+++ glicko_ranking_test.py
@@ -4,26 +4,51 @@ import numpy
 from datetime import *
 from dateutil.relativedelta import relativedelta, MO
 import math
+from math import sqrt
 
 def g(phi):
-    return 1/sqrt(1+3*phi_loser^2/math.pi)
+    return 1/sqrt(1+3*phi_loser**2/math.pi)
     
 def E(mu, muj, phij):
     return 1/(1+exp(-g(phij)*(mu-muj)))
 
 
-def glickoRanking(df, period, delta_volatility):
+def new_sigma_function(sigma, delta, phi, variance, tau, error):
+    a=math.log(sigma**2)
+    if delta**2 >= (phi**2+variance**2):
+        b=math.log(delta**2-phi**2-variance)
+    else:
+        k=1
+        while sigma_convergence_function((a-k*tau), sigma, delta, phi, variance, tau, error):
+            k=k+1
+        b=a-k*tau
+    fa=sigma_convergence_function(a, sigma, delta, phi, variance, tau, error)
+    fb=sigma_convergence_function(b, sigma, delta, phi, variance, tau, error)
+    while math.fabs(b-a)>error:
+        c=a+(a-b)*fa/(fb-fa)
+        fc=sigma_convergence_function(c, sigma, delta, phi, variance, tau, error)
+        if fc*fb<0:
+            a=b
+            fa=fb
+        else:
+            fa=fa/2
+        b=c
+        fb=fc
+    return math.exp(a/2)
+
+def glickoRanking(df, period, tau):
     """
     Given the list on matches in chronological order, for each match, computes 
     the glicko ranking of the 2 players at the beginning of the match
     The period is given in days
-    The delta_volatility is a constant to constrain the change of volatility over time
+    The tau is a constant to constrain the change of volatility over time
 
     The selected period is 7 days, following the ATP ranking updates, and the update occurs every mondays
     On each pass, all rankings are updated: for player who do not play, their RD increases
     
     """
     print("Glicko-2 rankings computing...")
+    print("System constant Tau set to: "+str(tau))
     players=list(pandas.Series(list(df.Winner)+list(df.Loser)).value_counts().index)
     # For an unrated player, we start with a rating of 1500, a RD of 350 and volatility of 0.06
     # This is the default value for all players
@@ -62,76 +87,94 @@ def glickoRanking(df, period, delta_volatility):
             
             # If this player played last week, we compute his new score
             lwp=list(pandas.Series(list(lwm.Winner)+list(lwm.Loser)).value_counts().index)
-            if player1 is in lwp:
-                p1Matches=lwp[lwp.Winner==player1|lwp.Loser==player1]
-                r=glickoRK[player1]
-                mu=glickoRD[player1]
+            if player1 in lwp:
+                p1Matches=lwm[(lwm.Winner==player1)|(lwm.Loser==player1)]
                 nu=0
                 delta=0
                 wnl=[]
                 for j in range(0,len(p1Matches)):
-                    mu_winner=(glickoRK[wp.iloc[i,:].Winner]-1500)/173.7178
-                    mu_loser=(glickoRK[wp.iloc[i,:].Loser]-1500)/173.7178
-                    phi_winner=glicko_winner[1]/173.7178
-                    phi_loser=glicko_loser[1]/173.7178
-                    if lwp.iloc[i,:].Winner==player1:
-                        nu+=(g(phi_loser)^2)*E(mu_winner,mu_loser,phi_loser)*(1-E(mu_winner,mu_loser,phi_loser))
+                    mu_winner=(glickoRK[p1Matches.iloc[j,:].Winner]-1500)/173.7178
+                    mu_loser=(glickoRK[p1Matches.iloc[j,:].Loser]-1500)/173.7178
+                    phi_winner=glickoRD[p1Matches.iloc[j,:].Winner]/173.7178
+                    phi_loser=glickoRD[p1Matches.iloc[j,:].Loser]/173.7178
+                    if p1Matches.iloc[j,:].Winner==player1:
+                        nu+=(g(phi_loser)**2)*E(mu_winner,mu_loser,phi_loser)*(1-E(mu_winner,mu_loser,phi_loser))
                         delta+=g(phi_loser)*(1-E(mu_winner,mu_loser,phi_loser))
-                    elif lwp.iloc[i,:].Loser==player1:
-                        nu+=(g(phi_winner)^2)*E(mu_loser,mu_winner,phi_winner)*(1-E(mu_loser,mu_winner,phi_winner))
+                    elif p1Matches.iloc[j,:].Loser==player1:
+                        nu+=(g(phi_winner)**2)*E(mu_loser,mu_winner,phi_winner)*(1-E(mu_loser,mu_winner,phi_winner))
                         delta+=g(phi_winner)*(-1-E(mu_loser,mu_winner,phi_winner))
                 nu=1/nu
                 delta=nu*delta
-                
+                # We now compute the new volatility
+                glickoSig[player1]=new_sigma_function(glickoSig[player1], delta, phi_winner, nu, tau, 0.000001)
+                # We update the rating and RD to the new values
+                glickoRD[player1]=1/sqrt((1/sqrt(glickoSig[player1]**2+(glickoRD[player1]/173.7178)**2))+(1/nu))
+                glickoRK[player1]=glickoRK[player1]+glickoRD[player1]**2*delta/nu
+                # We convert the values back to the glicko rating
+                glickoRD[player1]=glickoRD[player1]*173.7178
+                glickoRK[player1]=glickoRK[player1]*173.7178+1500
+            
             # If this player didn't play last week and isActive is False, we set isActive to True and do not change the score
-            
+            elif not(isActive[player1]):
+                isActive[player1]=True
             # If this player didn't play last week and isActive is True, we compute his new score
+            # in this case only the RD changes (increases)
+            else:
+                new_phi=glickoRD[player1]/173.7178
+                new_phi=sqrt(new_phi**2+glickoSig[player1]**2)
+                glickoRD[player1]=new_phi*173.7178
+
             
-            # For all the other players that did not play this week and whose isActive boolean is True, we compute the new score
-            #on parcours la liste complete et on compare si le joueur est dans la liste des matchs de cette semaine
-            # increment the week and start again!
+            # we do the same for player 2 now
+            if player2 in lwp:
+                p2Matches=lwm[(lwm.Winner==player2)|(lwm.Loser==player2)]
+                nu=0
+                delta=0
+                wnl=[]
+                for j in range(0,len(p2Matches)):
+                    mu_winner=(glickoRK[p2Matches.iloc[j,:].Winner]-1500)/173.7178
+                    mu_loser=(glickoRK[p2Matches.iloc[j,:].Loser]-1500)/173.7178
+                    phi_winner=glickoRD[p2Matches.iloc[j,:].Winner]/173.7178
+                    phi_loser=glickoRD[p2Matches.iloc[j,:].Loser]/173.7178
+                    if p2Matches.iloc[j,:].Winner==player2:
+                        nu+=(g(phi_loser)**2)*E(mu_winner,mu_loser,phi_loser)*(1-E(mu_winner,mu_loser,phi_loser))
+                        delta+=g(phi_loser)*(1-E(mu_winner,mu_loser,phi_loser))
+                    elif p2Matches.iloc[j,:].Loser==player2:
+                        nu+=(g(phi_winner)**2)*E(mu_loser,mu_winner,phi_winner)*(1-E(mu_loser,mu_winner,phi_winner))
+                        delta+=g(phi_winner)*(-1-E(mu_loser,mu_winner,phi_winner))
+                nu=1/nu
+                delta=nu*delta
+                # We now compute the new volatility
+                glickoSig[player2]=new_sigma_function(glickoSig[player2], delta, phi_winner, nu, tau, 0.000001)
+                # We update the rating and RD to the new values
+                glickoRD[player2]=1/sqrt((1/sqrt(glickoSig[player2]**2+(glickoRD[player2]/173.7178)**2))+(1/nu))
+                glickoRK[player2]=glickoRK[player2]+glickoRD[player2]**2*delta/nu
+                # We convert the values back to the glicko rating
+                glickoRD[player2]=glickoRD[player2]*173.7178
+                glickoRK[player2]=glickoRK[player2]*173.7178+1500
+
+            # If this player didn't play last week and isActive is False, we set isActive to True and do not change the score
+            elif not(isActive[player1]):
+                isActive[player1]=True
+            # If this player didn't play last week and isActive is True, we compute his new score
+            # in this case only the RD changes (increases)
+            else:
+                new_phi=glickoRD[player1]/173.7178
+                new_phi=sqrt(new_phi**2+glickoSig[player1]**2)
+                glickoRD[player1]=new_phi*173.7178
+            
+        # For all the other players that did not play this week and whose isActive boolean is True, we compute the new score
+        twp=list(pandas.Series(list(twm.Winner)+list(twm.Loser)).value_counts().index)
+        for n in range(0,len(players)):
+            player=players[n]
+            if(isActive[player] and (player not in twp)):
+                new_phi=glickoRD[player]/173.7178
+                new_phi=sqrt(new_phi**2+glickoSig[player]**2)
+                glickoRD[player]=new_phi*173.7178
+        
+        # increment the week and start again!
         df_this_week=df_this_week+timedelta(weeks=1)
-        
-    """
 
-    for i in range(1,len(data)):
-        winner=data.iloc[i-1,:].Winner
-        loser=data.iloc[i-1,:].Loser
-        # We retrieve the current player's rankings
-        glicko_winner=glicko[winner]
-        glicko_loser=glicko[loser]
-        # We convert the rating to the glicko scale
-        mu_winner=(glicko_winner[0]-1500)/173.7178
-        mu_loser=(glicko_loser[0]-1500)/173.7178
-        phi_winner=glicko_winner[1]/173.7178
-        phi_loser=glicko_loser[1]/173.7178
-
-        # Sigma remains the same
-        sigma_winner=glicko_winner[2]
-        sigma_loser=glicko_loser[2]
-
-        # We compute the change in rating now
-        variance_winner=1/(g_function(phi_winner)*e_function(mu_winner,mu_loser,phi_loser))
-        variance_loser=1/(g_function(phi_loser)*e_function(mu_loser,mu_winner,phi_winner))
-        rating_change_winner=variance_winner*g_function(phi_loser)*(1-e_function(mu_winner,mu_loser,phi_loser))
-        rating_change_loser=variance_loser*g_function(phi_winner)*(-e_function(mu_loser,mu_winner,phi_winner))
-        
-        # We now compute the new volatility
-        vol_winner=new_sigma_function(sigma_winner, rating_change_winner, phi_winner, variance_winner, delta_volatility, 0.000001)
-        vol_loser=new_sigma_function(sigma_loser, rating_change_loser, phi_loser, variance_loser, delta_volatility, 0.000001)
-        
-        # We update both player's rankings
-        glicko[winner]=((glicko_winner+rating_change_winner),variance_winner,vol_winner)
-        glicko[loser]=((glicko_loser+rating_change_loser),variance_loser,vol_loser)
-        
-        # We set the new glicko ranking on the next match of each player
-        glicko_ranking.append((glicko[data.iloc[i,:].Winner],glicko[data.iloc[i,:].Loser])) 
-        if i%500==0:
-            print(str(i)+" matches computed...")
-    glicko_ranking=pandas.DataFrame(glicko_ranking,columns=["glicko_winner","glicko_loser"])    
-    glicko_ranking["proba_glicko"]=1 / (1 + 10 ** ((ranking_elo["elo_loser"] - ranking_elo["elo_winner"]) / 400))   
-    data = pandas.concat([data,ranking_elo],1) 
-    """
     return df
 
 
